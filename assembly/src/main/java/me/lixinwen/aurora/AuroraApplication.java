8cb088fa7d8bbc9f8640f0ab9f74ab0c55c12727
sapronlee <sapronlee@gmail.com>
2016-11-23 15:55:59 +0800

    重新规划目录和项目结构

---
+++ assembly/src/main/java/me/lixinwen/aurora/AuroraApplication.java
@@ -0,0 +1,14 @@
+package me.lixinwen.aurora;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Created by chucklee on 2016/11/23.
+ */
+@SpringBootApplication
+public class AuroraApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(AuroraApplication.class, args);
+    }
+}
